a5674a91a27ab74c0f7fae0c7ed1f9e9ca0cd5ba
Jon Marcum <jrmarcum.se@gmail.com>
2026-01-05 17:25:21 -0500

    Delete fib directory

--- fib/target/wasm32-wasip1/debug/deps/fib-06b03e848496be33.d
+++
@@ -1,5 +0,0 @@
-D:\Programs\_ProgramExamples\Example_Programs\wasmExamples\wasm_wasi\fib\target\wasm32-wasip1\debug\deps\fib-06b03e848496be33.d: src\main.rs
-
-D:\Programs\_ProgramExamples\Example_Programs\wasmExamples\wasm_wasi\fib\target\wasm32-wasip1\debug\deps\fib-06b03e848496be33.wasm: src\main.rs
-
-src\main.rs:
--- fib/target/wasm32-wasip1/release/fib.d
+++
@@ -1,1 +0,0 @@
-D:\Programs\_ProgramExamples\Example_Programs\wasmExamples\wasm_wasi\fib\target\wasm32-wasip1\release\fib.wasm: D:\Programs\_ProgramExamples\Example_Programs\wasmExamples\wasm_wasi\fib\src\main.rs
